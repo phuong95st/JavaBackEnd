b07bac0e5ce9b4c0d751f67ab8d2a77d5e96b8ea
PhuongNH <phuongtran95st@gmail.com>
2022-04-06 21:41:00 +0700

    Move class Runner

--- src/basic/buoi3/Runner.java
+++ src/basic/buoi3/Runner.java
@@ -1,16 +1,16 @@
-package basic;
+package basic.buoi3;
 
 import basic.buoi3.Programmer;
 
 public class Runner {
     public static void main(String[] args) {
         // tạo đối tuong Programmer
-        Programmer programmer = new Programmer();
+        Programmer programmer = new Programmer("Phuong","Nguyen Huu", "Luvina JSC");
 
         programmer.eat();
         programmer.learn();
         programmer.walk();
         programmer.coding();
-        System.out.println("Name: "+programmer.getName() + ", desig: " + programmer.getDesignation() + "company: " + programmer.getCompanyName());
+        System.out.println("Name: "+programmer.getName() + ", desig: " + programmer.getDesignation() + ", company: " + programmer.getCompanyName());
     }
 }

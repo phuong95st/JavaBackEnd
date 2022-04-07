7e04e8dbbfcea11e69545d2ce105bc008e0104ef
PhuongNH <phuongtran95st@gmail.com>
2022-04-07 19:12:59 +0700

    Update Eatable.java

--- src/basic/buoi4/Eatable.java
+++ src/basic/buoi4/Eatable.java
@@ -14,12 +14,23 @@ interface Sleepable{
 
 class Humman implements Eatable,Walkable,Sleepable{
 
+    @Override
+    public void eating() {
+        System.out.println("Humman eating");
+    }
 }
 
 class Pig implements Eatable,Walkable,Sleepable{
 
     @Override
     public void eating() {
+        System.out.println("Pig eating");
+    }
+}
 
+class Main{
+    public static void main(String[] args) {
+        new Humman().eating();
+        new Pig().eating();
     }
 }

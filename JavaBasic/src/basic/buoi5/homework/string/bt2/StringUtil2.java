73f0f068b168c048ecb825f2456597ce70b91b04
phuongnh <phuongnh@ftl.vn>
2022-04-21 20:24:41 +0700

    update bt2 buoi 5, code nong dan

--- JavaBasic/src/basic/buoi5/homework/string/bt2/StringUtil2.java
+++ JavaBasic/src/basic/buoi5/homework/string/bt2/StringUtil2.java
@@ -1,5 +1,7 @@
 package basic.buoi5.homework.string.bt2;
 
+import basic.buoi5.homework.string.bt13.StringUtil13;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,9 +40,37 @@ public class StringUtil2
 		System.out.println(out);
 	}
 
+	public void countNumberOfChar2(String str)
+	{
+		// loop every char of str
+		String strLooped = "";
+		String out = "{";
+		for (int i = 0; i < str.length(); i++)
+		{
+			//System.out.println(strLooped.indexOf(String.valueOf(str.charAt(i))));
+
+			int count = 0;
+			if (strLooped.indexOf(String.valueOf(str.charAt(i))) < 0)
+			{
+				for (int j = i; j < str.length(); j++)
+				{
+					if (str.charAt(i) == str.charAt(j))
+					{
+						count++;
+					}
+				}
+				strLooped += String.valueOf(str.charAt(i));
+				out += String.format("'%c': %d,", str.charAt(i), count);
+			}
+			//System.out.println(strLooped);
+		}
+		System.out.println(out.substring(0, out.length() - 1) + "}");
+	}
+
 	public static void main(String[] args)
 	{
 		new StringUtil2().countNumberOfChar("google.com");
+		new StringUtil2().countNumberOfChar2("google.com");
 	}
 
 }
